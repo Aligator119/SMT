968f7a550b8968c3fc2bd5fd4799e5190ad1fd91
Aligator119 <vanja.irchak@gmail.com>
2014-04-28 18:06:52 +0300

    First commit

---
+++ SMT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SMT
+//
+//  Created by Mac on 4/28/14.
+//  Copyright (c) 2014 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SMTTests/SMTTests.m
@@ -0,0 +1,34 @@
+//
+//  SMTTests.m
+//  SMTTests
+//
+//  Created by Mac on 4/28/14.
+//  Copyright (c) 2014 Mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SMTTests : XCTestCase
+
+@end
+
+@implementation SMTTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
