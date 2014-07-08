d4e8e6612152fac0dbe123cb9c3755e8219e911d
Aligator119 <vanja.irchak@gmail.com>
2014-07-08 15:10:29 +0300

    add loader on cell

--- SMT/ViewControllers/FlyoutMenuViewController/ImageShow/ImageShow.h
+++ SMT/ViewControllers/FlyoutMenuViewController/ImageShow/ImageShow.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+
 @interface ImageShow : UICollectionViewCell
 @property (strong, nonatomic) IBOutlet UIImageView *img;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *heigthImage;
@@ -8,5 +9,5 @@
 - (void)setImageWithURL:(NSURL *) url andImageID:(NSString *) photoID andDescriptions:(NSString *)str;
 - (void)setPhotoDescriptions:(NSString *)str;
 - (void)setImage:(UIImage *)image;
-
+- (void)startLaderInCell;
 @end
